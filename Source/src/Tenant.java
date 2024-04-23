75fad12fd3c8d643e2f2651814937b8f6b29af11
Nam Kaito7love <104958975+kaito7love@users.noreply.github.com>
2024-04-23 14:57:01 +0700

    Lab2
    User and Property

---
+++ Source/src/Tenant.java
@@ -0,0 +1,11 @@
+
+public class Tenant {
+//	private 
+	
+	public void requestCreateRentalContract() {
+		
+	}
+	public void requestTerminateRentalContract() {
+		
+	}
+}
---
+++ Source/src/User.java
@@ -0,0 +1,18 @@
+
+public class User {
+	private String userID;
+	private String password;
+	private String email;
+	private UserType userType;
+	
+	public void CreateUser() {
+		
+	}
+	public void ResetPassword() {
+		
+	}
+}
+ enum UserType {
+	 PROPERTYONWER,
+	 TENANT;
+ }
